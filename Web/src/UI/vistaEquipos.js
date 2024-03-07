3eef700cac22c608516b6b61474847cde9cd1193
Nahuel Stella <nahuelprg@gmail.com>
2024-03-07 12:06:04 -0300

    otros retoques esteticos

--- Web/src/UI/vistaEquipos.js
+++ Web/src/UI/vistaEquipos.js
@@ -16,7 +16,7 @@ export async function mostrarEquipos(equipos){
                 <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                     <button type="button" class="btn btn-sm btn-primary btn-outline-secondary"><a href="${equipo.website}"><strong>Sitio Web</strong></a></button>
-                    <a type="button" class="btn btn-sm btn-primary btn-outline-secondary" href="Web/Templates/mostrar_equipo?id=${equipo.id}"><strong>Ver Equipo</strong></a>
+                    <a type="button" id="btnLink" class="btn btn-sm btn-primary btn-outline-secondary" href="Web/Templates/mostrar_equipo?id=${equipo.id}"><strong>Ver Equipo</strong></a>
                 </div>
                 </div>
             </div>
